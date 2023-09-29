8bb3aef980b9709525800536987e9d16aa999665
Deepak Choudhary <deepak.choudhary03@ad.infosys.com>
2023-09-29 19:09:51 +0530

    new code

--- async-await.js
+++ async-await.js
@@ -1,7 +1,25 @@
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("promise is resolved");
+  }, 5000);
+});
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise is resolved");
   }, 10000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise is resolved");
+  }, 5000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise is resolved");
+  }, 15000);
 });
 
 // function getData() {
@@ -13,10 +31,10 @@ const p = new Promise((resolve, reject) => {
 // getData();
 
 async function getData1() {
-  const val = await p;
+  const val = await p1;
   console.log("hello1");
   console.log(val);
-  const val2 = await p;
+  const val2 = await p2;
   console.log("hello 2");
   console.log(val2);
 }
---
+++ random.js
@@ -0,0 +1,4 @@
+const a = { firstName: "john", lastName: "cena" };
+const b = { firstName: "john", lastName: "cena" };
+
+console.log(a === b);
