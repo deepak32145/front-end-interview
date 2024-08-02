2843f248ab1152f10986f8334e3edff303f60a4e
Deepak Choudhary <deepak.choudhary03@ad.infosys.com>
2024-08-02 19:59:27 +0530

    new code added

---
+++ javaScriptCore/first.js
@@ -0,0 +1,71 @@
+/*
+ ****** First problem *****
+ */
+
+// {
+//   let a = 5;
+//   let b = 10;
+// }
+
+// console.log(a);
+// console.log(b);
+
+/*
+ ****** second problem *****
+ */
+
+// function test() {
+//   var a = "hello";
+//   let b = "go";
+
+//   if (true) {
+//     let a = "Hi";
+//     var b = "don't go";
+//     console.log(a);
+//     console.log(b);
+//   }
+//   console.log(a);
+//   console.log(b);
+// }
+
+// test();
+
+/*
+ ****** Third problem *****
+ */
+
+// for (var i = 0; i < 5; i++) {
+//   function inner(i) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   inner(i);
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+/*
+ ****** fourth problem *****
+ */
+
+// const a = { firstName: "john", lastName: "cena" };
+// const b = { firstName: "john", lastName: "cena" };
+
+// console.log(a == b);
+
+// console.log(a === b);
+
+/*
+ ****** fifth problem *****
+ */
--- random.js
+++ random.js
@@ -4,3 +4,7 @@ const b = { firstName: "john", lastName: "cena" };
 console.log(a == b);
 
 console.log(a === b);
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
