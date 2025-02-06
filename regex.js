388cec5cfb8b116b794610a177d78ab41e44da2c
Deepak Choudhary <deepak32145@gmail.com>
2025-02-06 15:29:24 +0530

    regex

---
+++ regex.js
@@ -0,0 +1,14 @@
+const ssnRegex =  /^(?!([0-9])\1{2}-\1{2}-\1{4})\d{3}-\d{2}-\d{4}$/;
+;
+
+
+
+
+
+function validateSSN(ssn) {
+    return ssnRegex.test(ssn);
+}
+
+console.log(validateSSN("123-45-6789")); // ✅ true (Valid)
+console.log(validateSSN("000-12-3456")); // ❌ false (Invalid)
+console.log(validateSSN("111-11-1111")); // ❌ false (All same digits)
