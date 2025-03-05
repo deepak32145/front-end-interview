efd916bae4623dc390d6acff34025cd97ba9ff8d
Deepak Choudhary <deepak32145@gmail.com>
2025-03-05 13:14:41 +0530

    check

--- regex.js
+++ regex.js
@@ -1,5 +1,35 @@
+async function importAllTests() {
+  const testFiles = Object.keys((globalThis as any).__webpack_modules__).filter((path) =>
+    /\.spec\.ts$/.test(path)
+  );
 
-const testFiles = Object.keys((globalThis as any).__webpack_modules__)
-  .filter((path) => /\.spec\.ts$/.test(path));
+  for (const file of testFiles) {
+    await import(/* @vite-ignore */ file);
+  }
+}
 
-testFiles.forEach((file) => __webpack_require__(file));
+importAllTests();
+
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    files: [
+      { pattern: 'src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      'src/test.ts': ['webpack']
+    },
+    webpack: {
+      mode: 'development',
+      resolve: {
+        fallback: {
+          "fs": false,
+          "path": false
+        }
+      }
+    },
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
