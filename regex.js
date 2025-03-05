ce7e8ab2c5e53d4357c24f17895d00747280d1cf
Deepak Choudhary <deepak32145@gmail.com>
2025-03-05 16:21:08 +0530

    check

--- regex.js
+++ regex.js
@@ -1,5 +1,16 @@
+npm install --save-dev \
+  webpack@5 webpack-cli@5 webpack-dev-server@4 \
+  karma-webpack@5 ts-loader@9 \
+  karma karma-chrome-launcher \
+  karma-jasmine karma-jasmine-html-reporter \
+  @angular-devkit/build-angular
+
+
+  const webpack = require('webpack');
+
 module.exports = function (config) {
   config.set({
+    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     files: [
       { pattern: 'src/test.ts', watched: false }
@@ -9,23 +20,29 @@ module.exports = function (config) {
     },
     webpack: {
       mode: 'development',
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
       resolve: {
-        extensions: ['.ts', '.js'],
         fallback: {
-          fs: false,
-          path: false,
-        },
+          "crypto": false,
+          "path": false,
+          "fs": false
+        }
       },
+      plugins: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+      ],
     },
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    plugins: [
+      require('karma-webpack'),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     browsers: ['Chrome'],
     singleRun: false,
   });
