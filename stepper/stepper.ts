2834415ae00cbf69a14d478640c3b45e09cc6c83
deepak choudhary <deepak32145@gmail.com>
2025-04-16 12:33:39 +0530

    Update stepper.ts

--- stepper/stepper.ts
+++ stepper/stepper.ts
@@ -1,70 +1,61 @@
-import { Component, Inject, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Inject , AfterViewInit , ViewChild, viewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatStepper } from '@angular/material/stepper';
 
 @Component({
-  selector: 'app-stepper-form',
-  templateUrl: './stepper-form.component.html',
-  styleUrls: ['./stepper-form.component.scss']
+  selector: 'app-lines-loans-dialog',
+  templateUrl: './lines-loans-dialog.component.html',
+  styleUrl: './lines-loans-dialog.component.css'
 })
-export class StepperFormComponent implements OnInit, AfterViewInit {
-  @ViewChild('stepper') stepper!: MatStepper;
-
-  firstFormGroup: FormGroup;
+export class LinesLoansDialogComponent implements OnInit , AfterViewInit {
+ @ViewChild('stepper') stepper!: MatStepper;
+  firstFormGroup!: FormGroup;
   apiResponse: any;
   skipFirstStep = false;
 
+
   constructor(
-    private fb: FormBuilder,
+    private _formBuilder: FormBuilder,
     private http: HttpClient,
-    public dialogRef: MatDialogRef<StepperFormComponent>,
+    private dialogRef: MatDialogRef<LinesLoansDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.firstFormGroup = this.fb.group({
+    this.firstFormGroup = this._formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
-      age: ['', Validators.required],
-      city: ['', Validators.required]
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      age : ['', Validators.required],
     });
   }
-
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.data?.skipFirstStep) {
       this.skipFirstStep = true;
     }
   }
-
   ngAfterViewInit() {
+    console.log('stepper', this.stepper);
     if (this.skipFirstStep) {
-      setTimeout(() => this.stepper.selectedIndex = 1, 0);
+      //setTimeout(() => this.stepperRef.selectedIndex = 1, 0);
     }
   }
 
-  goToSecondStep(stepper: MatStepper) {
-    if (this.firstFormGroup.invalid) {
-      this.firstFormGroup.markAllAsTouched();
-      return;
-    }
-
-    const payload = this.firstFormGroup.value;
-
-    this.http.post('https://jsonplaceholder.typicode.com/posts', payload).subscribe({
-      next: (res) => {
-        this.apiResponse = res;
-        stepper.next();
-      },
-      error: (err) => {
-        console.error('API error:', err);
-      }
-    });
+  goToSecondStep() {
+    console.log('stepper', this.stepper);
+    this.stepper.next();
   }
 
   submit() {
-    console.log('Final Submit', this.apiResponse);
-    this.dialogRef.close(this.apiResponse);
+    alert('Form submitted!');
+    this.dialogRef.close({ status: 'submitted', formData: this.firstFormGroup.value });
   }
+
+  close() {
+    this.dialogRef.close();
+  }
+
 }
