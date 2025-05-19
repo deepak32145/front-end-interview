542bfb512d22d775027de0fe2bb7b1ff1deaa077
Deepak Choudhary <deepak32145@gmail.com>
2025-05-19 11:39:33 +0530

    code

--- regex.js
+++ regex.js
@@ -1,17 +1,1 @@
-function makeCounter() {
-    let count = 0;
-    return {
-      inc: () => ++count,
-      dec: () => --count,
-      get: () => count
-    };
-  }
-  
-  const c1 = makeCounter();
-  const c2 = makeCounter();
-  
-  c1.inc();
-  c1.inc();
-  c2.inc();
-  
-  console.log(c1.get(), c2.get());
+compareIndustries = (a: any, b: any) => a && b ? a.value === b.value : a === b;
